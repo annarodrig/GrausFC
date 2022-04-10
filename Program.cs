af0161e8b48fbe6ed2adb5d81b729d1fba006d1f
= <=>
2022-04-10 12:11:48 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double c,f;
+
+Console.Write("Digite uma temperatura em graus F°:");
+f = Convert.ToDouble (Console.ReadLine());
+
+c = (f - 32) / 1.8;
+
+Console.WriteLine($"{f:N2}° equivalem a {c:N2}°");
